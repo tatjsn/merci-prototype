274f65fb8c6c9ae2f2dc1736c3d4165034605c70
Taz Jackson <tatjsn@users.noreply.github.com>
2016-11-25 01:30:14 +0900

    Introduce getTree

--- get-category-tree.js
+++ get-category-tree.js
@@ -4,27 +4,38 @@ const config = require('dotenv').config();
 const isChildOf = id => ({ fields }) =>
   fields.parentCategory && fields.parentCategory.sys.id === id;
 
+const getProducts = categoryId =>
+  fetch(`${config.SPACE_URL}/entries?access_token=${config.ACCESS_TOKEN}&content_type=product&` +
+    `fields.category.sys.id=${categoryId}`)
+    .then(res => res.json())
+    .then(data => data.items.map(item => {
+      const { id } = item.sys;
+      const { name } = item.fields;
+      return { id, name };
+    }))
+    .catch(error => {
+      console.log(error.stack);
+    });
+
+const getTree = (data, rootItem) => {
+  const { id } = rootItem.sys;
+  const { name } = rootItem.fields;
+  const children = data.items
+    .filter(isChildOf(id))
+    .map(item => getTree(data, item));
+  const products = []; // TODO
+  return { id, name, children, products };
+};
+
 fetch(`${config.SPACE_URL}/entries?access_token=${config.ACCESS_TOKEN}&content_type=category`)
   .then(res => res.json())
   .then(data => {
     const root = data.items.find(item => item.fields.name === 'Root');
-    console.log('Root', root.sys.id);
-    const firstLevelChildren = data.items.filter(isChildOf(root.sys.id));
-    firstLevelChildren.forEach(firstLevel => {
-      console.log('  ', firstLevel.fields.name, firstLevel.sys.id);
-      const secondLevelChildren = data.items.filter(isChildOf(firstLevel.sys.id));
-      secondLevelChildren.forEach(secondLevel => {
-        console.log('    ', secondLevel.fields.name, secondLevel.sys.id);
-      });
-    });
-    return '3MH3ZUk2k8mUWKM80MckCK'; // an id of second level child, just for idea
+    return getTree(data, root);
   })
-  .then(categoryId => fetch(`${config.SPACE_URL}/entries?access_token=${config.ACCESS_TOKEN}&content_type=product&` +
-    `fields.category.sys.id=${categoryId}`))
-  .then(res => res.json())
-  .then(data => {
-    console.log('----');
-    data.items.forEach(item => {
-      console.log(item.fields.name, item.sys.id);
-    });
+  .then(root => {
+    console.log(JSON.stringify(root));
+  })
+  .catch(error => {
+    console.log(error.stack);
   });
